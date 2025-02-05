381a2d8a11eb3c74943ced89f60948f858550b10
Christopher <topitops123@gmail.com>
2025-02-05 17:30:01 +0800

    Update project file paths and configuration

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd48d0b53cc15035e02b811fe7d71041c32b17c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c11b4ca5f10ce43431b82f632e362ea87a07a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
